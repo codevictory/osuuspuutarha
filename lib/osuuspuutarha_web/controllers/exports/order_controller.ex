090053c312ce4cb73dcfdfbc88064aa539ba4d5a
Veikko Lintujärvi <veikko@lintujarvi.fi>
2023-06-08 20:46:36 +0300

    Fix typo in filename

--- lib/osuuspuutarha_web/controllers/exports/order_controller.ex
+++ lib/osuuspuutarha_web/controllers/exports/order_controller.ex
@@ -8,7 +8,7 @@ defmodule OsuuspuutarhaWeb.Exports.OrderController do
 
     conn
     |> put_resp_content_type("text/xlsx")
-    |> put_resp_header("content-disposition", "attachment; filename=\"tilaukset.xlxs\"")
+    |> put_resp_header("content-disposition", "attachment; filename=\"tilaukset.xlsx\"")
     |> render("tilaukset.xlsx", %{orders: orders})
   end
 end
