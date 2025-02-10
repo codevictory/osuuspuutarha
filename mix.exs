138708047c88d43bbd30b41e3ff0aeb0b07c820f
Veikko Lintujärvi <veikko@lintujarvi.fi>
2025-02-10 21:06:57 +0200

    Added credo as dev and test dependency

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Osuuspuutarha.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:elixlsx, "~> 0.5.1"}
+      {:elixlsx, "~> 0.5.1"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
