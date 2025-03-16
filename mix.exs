2f0d028332b3949b63d442ac228f1e57ecea4f50
Veikko Lintujärvi <veikko@lintujarvi.fi>
2025-03-16 21:12:53 +0200

    Bump up version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Osuuspuutarha.MixProject do
   def project do
     [
       app: :osuuspuutarha,
-      version: "0.1.0",
+      version: "1.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
